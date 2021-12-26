371acf29d763276941b87650d487d645b99967c6
Mario <mamerida2013@gmail.com>
2021-12-26 17:15:59 -0300

    componentes controlados comprobacion de varios inputs con un solo useState, manejo de objetos literales y distintas formas de manejar los valores de useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import { useState } from "react"
 
 const App = () =>{
-    const[ value, setValue ] = useState("")
+    //para poder manejar varios inputs de manera simultanea sin necesidad de duplicar el useState paso un objeto al valor inicial con
+    //los distintos nombres de los inputs para que asi en el handleChange dependiendo del nombre que lo invoco guarda uno y otro
+    const[ value, setValue ] = useState({normal: '' , texto: '' }) // para darle valores iniciales los paso aca 
     const handleChange = (e) =>{
-        setValue(e.target.value)
+
+        // con e.target.name puedo revisar el nombre del input que llama a la funcion 
+        // en este caso mediante las llaves digo que la propiedad va a ser dinamica y dependiendo del nombre sera donde se asigne el valor 
+        // setValue({[e.target.name]:e.target.value}) al dejarlo de esta manera se elimina la propiedad que no esta siendo completada 
+        // de las siguientes dos maneras se soluciona, lo que se hace es se concatena al value anterior la modificacion pasada en el front 
+        // setValue({...value,[e.target.name]:e.target.value}) 
+        setValue((state) => ({
+            ...state,
+            [e.target.name]:e.target.value
+        }))
     }
     console.log(value)
     return(
@@ -11,7 +22,8 @@ const App = () =>{
             {/* gracias al useState puedo hacer validaciones en tiempo real  */}
             {value.length < 5 ? <span>Longitud minima de 5</span> : null}
             {/* en un componente controlado tengo que pasarle el valor que va a manejar junto con una funcion onChange que maneje los cambios  */}
-            <input name="normal" type="text" value={value} onChange={handleChange}/>
+            <input name="normal" placeholder="Hola mundo "type="text" value={value.normal} onChange={handleChange}/><br/><br/>
+            <textarea name="texto" value={value.texto} onChange={handleChange}/>
         </div>
     )
 }
